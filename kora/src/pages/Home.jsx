8414a19247501f3a0f351534e2a6d20952e922b5
Esme <esmeralda.medina.p@ucb.edu.bo>
2026-03-20 18:47:26 -0400

    App y main

---
+++ kora/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// 
+// import Header from '../components/Header';
+// import Hero from '../components/Hero';
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Header /> */}
+      {/* <Hero /> */}
+      {/* <Features /> */}
+      {/* <Topics /> */}
+      {/* <Security /> */}
+      {/* <About /> */}
+      {/* <CTA /> */}
+      {/* <Footer /> */}
+      <h1 style={{ textAlign: 'center', marginTop: '50px', color: 'var(--color-primary)' }}>
+        Estructura de Kora Lista 🐼
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
